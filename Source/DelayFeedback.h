3def67d5a4a5064acac04e0dd08ed5713ced0b63
kweiwen <kweiwen.tseng@gmail.com>
2021-01-18 20:43:38 +0800

    add comment

--- Source/DelayFeedback.h
+++ Source/DelayFeedback.h
@@ -38,9 +38,13 @@ public:
 template <typename T>
 T DelayFeedback<T>::process(double input, float timeCtrl, float feedbackCtrl, float mixCtrl)
 {
+    // load dry signal from channelData
     auto drySignal = input;
+    // read wet signal from delay line
     auto wetSignal = digitalDelayLine.readBuffer(timeCtrl);
+    // sum up the dry signal + wet signal and write in the dely line
     digitalDelayLine.writeBuffer(drySignal + wetSignal * feedbackCtrl);
+    // adjust the dry and wet portion, return the value
     return wetSignal * mixCtrl + drySignal * (1 - mixCtrl);
 }
 
